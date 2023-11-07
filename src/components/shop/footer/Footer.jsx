02c31de23c98f17b432791cd153611c9609e0104
Alex Kruglov <lime4512@mail.ru>
2023-11-08 01:31:47 +0300

    add footer

--- src/components/shop/footer/Footer.jsx
+++ src/components/shop/footer/Footer.jsx
@@ -1,4 +1,7 @@
 import './Footer.css'
+import imgGitHub from './icons8-github-48.png'
+import imgVK from './icons8-vk-50.png'
+import imgTelegram from './icons8-telegram-50.png'
 
 const Footer = () => {
 	return (
@@ -12,8 +15,23 @@ const Footer = () => {
 			</p>
 
 			<div className='footer-nav'>
-				<a href=''>Гид-хаб</a>
-				<a href=''>ВК</a>
+				<ul className='footer-nav-list'>
+					<li>
+						<a href='https://t.me/lime4512'>
+							<img src={imgGitHub} alt='' />
+						</a>
+					</li>
+					<li>
+						<a href='https://vk.com/lime4512'>
+							<img src={imgVK} alt='' />
+						</a>
+					</li>
+					<li>
+						<a href='https://github.com/lime4512'>
+							<img src={imgTelegram} alt='' />
+						</a>
+					</li>
+				</ul>
 			</div>
 		</footer>
 	)
